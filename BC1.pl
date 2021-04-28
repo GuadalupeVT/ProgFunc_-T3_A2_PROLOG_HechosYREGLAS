763da91a0300aa05c38a4eca381d859e9d1a92bc
Guadalupe Vazquez <guvadlt@gmail.com>
2021-04-28 12:50:20 -0500

    Se creo el repositorio y se agrego la primera base de conocimientos

---
+++ BC1.pl
@@ -0,0 +1,13 @@
+/* 
+  El elefante es mas grande que un caballo
+  El caballo es mas grande que un perro
+  El perro es mas grande que un ratón
+*/
+masgrande(elefante,caballo).
+masgrande(caballo,perro).
+masgrande(perro,raton).
+
+/* El oso es más grande que el perro y que el raton*/
+   
+% 2        1     3     4%
+masgrande(oso,perro,raton).
